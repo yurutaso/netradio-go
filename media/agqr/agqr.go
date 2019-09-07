9dfbc869aa1987566cb3a41b9c2810b66dd71c9d
yurutaso <shrimp1205.physics@gmail.com>
2019-09-07 22:52:11 +0900

    fix agqr

--- media/agqr/agqr.go
+++ media/agqr/agqr.go
@@ -33,7 +33,9 @@ func Download(fileout, duration string) error {
 		return err
 	}
 
-	cmd := exec.Command(`rtmpdump`, `-r`, `rtmp://fms-base1.mitene.ad.jp/agqr/aandg22`, `--live`, `-o`, fileout)
+	//cmd := exec.Command(`rtmpdump`, `-r`, `rtmp://fms-base1.mitene.ad.jp/agqr/aandg22`, `--live`, `-o`, fileout)
+	// 2019/09/07
+	cmd := exec.Command(`rtmpdump`, `-r`, `rtmp://fms-base1.mitene.ad.jp/agqr/aandg1`, `--live`, `-o`, fileout)
 	if err := cmd.Start(); err != nil {
 		return err
 	}
